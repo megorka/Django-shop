7546e4c43c78cb7e2a836b2adfe3877062412a85
megorka <mikulevicegor@gmail.com>
2024-04-24 21:52:38 +0300

    Initial commit

---
+++ store/products/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'products/index.html')
+def products(request):
+    return render(request, 'products/products.html')
